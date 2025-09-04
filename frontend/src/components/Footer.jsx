094ba77014b72c7ee5b7aa6ff8b3889903be0cda
maitri-dalvi <maitridalvi2004@gmail.com>
2025-09-04 14:10:59 +0530

    footer c

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -26,7 +26,7 @@ const Footer = () => {
             <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
             <ul className='flex flex-col gap-1 text-gray-600'>
                 <li>+91 7385533173</li>
-                <li>abhaydandge333@gmail.com</li>
+                <li>glimmer@gmail.com</li>
             </ul>
         </div>
 
